da965c070dc4105a701091cd1303b6c949fa5798
Yuti Thakkar <thakkaryuti@gmail.com>
2023-03-08 20:51:24 -0500

    adds index.js

--- index.js
+++ index.js
@@ -8,23 +8,17 @@ function shout(string) {
   }
   function logShout(string) {
     console.log(string.toUpperCase());
-
   }
   function logWhisper(string) {
     console.log(string.toLowerCase());
-
   }
-  
   function sayHiToHeadphonedRoommate(string) {
-    if(string==string.toLowerCase){
-        return "I can't hear you!";
+    if(string===string.toLowerCase()) {
+      return "I can't hear you!";
     }
-    else if(string==string.toUpperCase) {
-        return "YES INDEED!";
-
+    else if (string===string.toUpperCase()){
+    return "YES INDEED!";
+    } else if (string=="Let's have dinner together!"){
+      return "I would love to!";
     }
-    else if(string=="Let's have dinner together!") {
-        return "I would love to!";
-    }
-console.log(sayHiToHeadphonedRoommate(string));
   }
